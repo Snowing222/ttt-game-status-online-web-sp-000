a8539b624e1069881a6852230fc9f8a1d23336f6
elsa chen <snowing222@gmail.com>
2020-05-11 14:48:20 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,12 +4,21 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+<<<<<<< HEAD
 WIN_COMBINATIONS=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
 
 def won?(board)
 
   WIN_COMBINATIONS.detect do |win_combination|
+=======
+
+
+def won?(board)
+  WIN_COMBINATIONS=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+
+  WIN_COMBINATIONS.each do |win_combination|
+>>>>>>> 99c32a7cdec70a1a479f0a6f5b574898bde9b2d7
     win_index_1=win_combination[0]
     win_index_2=win_combination[1]
     win_index_3=win_combination[2]
@@ -18,6 +27,7 @@ def won?(board)
     position_2=board[win_index_2]
     position_3=board[win_index_3]
 
+<<<<<<< HEAD
     position_1==position_2&&position_2==position_3&&position_taken?(board,win_index_1)
 
   end
@@ -39,3 +49,12 @@ def winner(board)
   won?(board)? board[won?(board)[0]]:nil
 
 end
+=======
+    if position_1=="X"&&position_2=="X"&&position_3=="X"
+      return win_combination
+    else
+      nil
+    end
+  end
+end
+>>>>>>> 99c32a7cdec70a1a479f0a6f5b574898bde9b2d7
